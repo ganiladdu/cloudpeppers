bcd3f3b85f75568c993deba3b6f7175124a29a20
naresh <naresh@gmail.com>
2019-06-07 16:24:01 +0530

    uploading

---
+++ src/main/java/com/example/GitCloudPeppars/GitCloudPepparsApplication.java
@@ -0,0 +1,13 @@
+package com.example.GitCloudPeppars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitCloudPepparsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitCloudPepparsApplication.class, args);
+	}
+
+}
