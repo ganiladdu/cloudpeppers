1562fba2a257e220b9bdc940bdc6751fa92abc67
naresh <naresh@gmail.com>
2019-06-07 20:08:11 +0530

    test2

--- src/main/java/com/example/GitCloudPeppars/GitCloudPepparsApplication.java
+++ src/main/java/com/example/GitCloudPeppars/GitCloudPepparsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitCloudPepparsApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Main11");
+		System.out.println("Main11222");
 		SpringApplication.run(GitCloudPepparsApplication.class, args);
 	}
 
