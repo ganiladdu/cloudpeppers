299b731fd9ad5de968abc054864e849befd6b6be
naresh <naresh@gmail.com>
2019-06-07 17:11:09 +0530

    sysout added

--- src/main/java/com/example/GitCloudPeppars/GitCloudPepparsApplication.java
+++ src/main/java/com/example/GitCloudPeppars/GitCloudPepparsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitCloudPepparsApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Main");
 		SpringApplication.run(GitCloudPepparsApplication.class, args);
 	}
 
